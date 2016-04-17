b10a5ddbd3a73b0cdbb2b80fec9eba5e25cdb2e6
David Brodsky <davidpbrodsky@gmail.com>
2016-04-17 13:30:30 -0700

    fix web_server read json request body

--- web_server.py
+++ web_server.py
@@ -4,16 +4,22 @@ from subprocess import Popen
 from arrive import unlock
 
 from SECRETS import HTTPS_API_KEY, SSL_CERT_PEM, SSL_KEY_PEM
+from support.logger import get_logger
 
 app = flask.Flask(__name__)
+
+logger = get_logger("web_server")
 
 
 @app.route("/arrive", methods=['POST'])
 def arrive():
-    if flask.request.values.get('token', default=None) == HTTPS_API_KEY:
+    json = flask.request.get_json()
+    token = json.get('token', None)
+    if token == HTTPS_API_KEY:
         Popen(["python3", "./arrive.py", "run"])  # async
         return "arrive"
     else:
+        logger.warn("Request provided invalid token: %s", flask.request.values)
         flask.abort(403)
 
 
