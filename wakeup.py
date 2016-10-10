1ed770c924d677e7804629c14aa1888dffc02716
David Brodsky <davidpbrodsky@gmail.com>
2016-10-10 07:32:46 -0700

    wakeup routine not dependent on no other rooms lit

--- wakeup.py
+++ wakeup.py
@@ -14,11 +14,6 @@ hall = settings.ROOMS[1]
 kitchen = settings.ROOMS[2]
 bedroom = settings.ROOMS[3]
 
-if bedroom.is_lit() or living_room.is_lit() or kitchen.is_lit() or hall.is_lit():
-    logger.info('Aborting wakeup, room is already lit. Bed %r, Living %r, Kitchen %r, Hall %r' %
-                (bedroom.is_lit(), living_room.is_lit(), kitchen.is_lit(), hall.is_lit()))
-    quit()
-
 command = {'xy': LIGHT_EVENING_XY, 'bri': 1, 'on': True}
 
 bedroom.update(command)
