d612b4d79b91eed6a30654e1eaebc513e88e1063
David Brodsky <dbro@dbro.pro>
2017-12-10 17:53:33 -0800

    Remove web server ssl serving
    ssl over LAN is :(

--- web_server.py
+++ web_server.py
@@ -5,7 +5,6 @@ import copy
 import flask
 import re
 
-from SECRETS import HTTPS_API_KEY, SSL_CERT_PEM, SSL_KEY_PEM
 from gevent import pywsgi
 from settings import ROOMS
 from support import env
@@ -19,11 +18,6 @@ app = flask.Flask(__name__)
 logger = get_logger("web_server")
 
 party_process = None  # Party mode process
-
-
-def is_local_request(request):
-    request_remote_addr = request.environ['REMOTE_ADDR']
-    return request_remote_addr.startswith('192.168.1.')
 
 
 def get_home_status(template_friendly_dict: bool) -> dict:
@@ -50,9 +44,6 @@ def get_home_status(template_friendly_dict: bool) -> dict:
 
 @app.route("/party-mode", methods=['POST'])
 def party():
-    if not is_local_request(flask.request):
-        flask.abort(404)
-
     global party_process
     json = flask.request.get_json()
     enabled = json.get('enabled', False)
@@ -87,93 +78,70 @@ def lights():
         ]
     }
     """
-    if is_local_request(flask.request):
-        json = flask.request.get_json()
-        rooms = json.get('rooms', [])
-        logger.info('Switching rooms %s', rooms)
-        for json_room in rooms:
-            room_name = json_room['name']
-            on_state = json_room['on']
+    json = flask.request.get_json()
+    rooms = json.get('rooms', [])
+    logger.info('Switching rooms %s', rooms)
+    for json_room in rooms:
+        room_name = json_room['name']
+        on_state = json_room['on']
 
-            for room in ROOMS:
-                # Strip whitespace
-                if room.name == room_name:
-                    logger.info('Switching room %s', room.name)
-                    room.switch(on_state)
-        return "Light commands sent."
-    else:
-        logger.info('Lights accessed by remote address %s', flask.request.environ['REMOTE_ADDR'])
-        flask.abort(404)
+        for room in ROOMS:
+            # Strip whitespace
+            if room.name == room_name:
+                logger.info('Switching room %s', room.name)
+                room.switch(on_state)
+    return "Light commands sent."
 
 
 @app.route("/guest-mode", methods=['POST'])
 def guest_mode():
-    if is_local_request(flask.request):
-        json = flask.request.get_json()
-        enabled = json.get('enabled', False)
-        env.set_guest_mode(enabled)
-        return "Guest mode is now %r" % enabled
-    else:
-        logger.info('Guest Mode accessed by remote address %s', flask.request.environ['REMOTE_ADDR'])
-        flask.abort(404)
+    json = flask.request.get_json()
+    enabled = json.get('enabled', False)
+    env.set_guest_mode(enabled)
+    return "Guest mode is now %r" % enabled
 
 
 @app.route("/motion-mode", methods=['POST'])
 def motion_mode():
-    if is_local_request(flask.request):
-        json = flask.request.get_json()
-        enabled = json.get('enabled', False)
-        env.set_motion_enabled(enabled)
-        return "Motion mode is now %r" % enabled
-    else:
-        logger.info('Motion Mode accessed by remote address %s', flask.request.environ['REMOTE_ADDR'])
-        flask.abort(404)
+    json = flask.request.get_json()
+    enabled = json.get('enabled', False)
+    env.set_motion_enabled(enabled)
+    return "Motion mode is now %r" % enabled
 
 
 @app.route("/vacation-mode", methods=['POST'])
 def vacation_mode():
-    if is_local_request(flask.request):
-        json = flask.request.get_json()
-        enabled = json.get('enabled', False)
-        env.set_vacation_mode(enabled)
-        return "Vacation mode is now %r" % enabled
-    else:
-        logger.info('Vacation Mode accessed by remote address %s', flask.request.environ['REMOTE_ADDR'])
-        flask.abort(404)
+    json = flask.request.get_json()
+    enabled = json.get('enabled', False)
+    env.set_vacation_mode(enabled)
+    return "Vacation mode is now %r" % enabled
 
 
 @app.route("/home-state", methods=['GET'])
 def home_status():
-    if is_local_request(flask.request):
-        home_status = get_home_status(template_friendly_dict=False)  # bools safe for json
-        return json.dumps(home_status)
-    else:
-        flask.abort(404)
+    home_status = get_home_status(template_friendly_dict=False)  # bools safe for json
+    return json.dumps(home_status)
 
 
 @app.route("/", methods=['GET'])
 def home():
-    if is_local_request(flask.request):
-        home_status_json = get_home_status(template_friendly_dict=False)  # Template engine requires string values
-        guest_mode = 'true' if env.is_guest_mode() else 'false'
-        party_mode = 'true' if env.is_party_mode() else 'false'
-        motion_mode = 'true' if env.is_motion_enabled() else 'false'
-        vacation_mode = 'true' if env.is_vacation_mode() else 'false'
+    home_status_json = get_home_status(template_friendly_dict=False)  # Template engine requires string values
+    guest_mode = 'true' if env.is_guest_mode() else 'false'
+    party_mode = 'true' if env.is_party_mode() else 'false'
+    motion_mode = 'true' if env.is_motion_enabled() else 'false'
+    vacation_mode = 'true' if env.is_vacation_mode() else 'false'
 
-        return flask.render_template('home.html',
-                                     home_status=home_status_json,
-                                     guest_mode=guest_mode,
-                                     party_mode=party_mode,
-                                     motion_mode=motion_mode,
-                                     vacation_mode=vacation_mode)
-    else:
-        logger.info('Home accessed by remote address %s', flask.request.environ['REMOTE_ADDR'])
-        flask.abort(404)
+    return flask.render_template('home.html',
+                                 home_status=home_status_json,
+                                 guest_mode=guest_mode,
+                                 party_mode=party_mode,
+                                 motion_mode=motion_mode,
+                                 vacation_mode=vacation_mode)
 
 
 if __name__ == "__main__":
 
     env.set_party_mode(False)
 
-    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app, keyfile=SSL_KEY_PEM, certfile=SSL_CERT_PEM)
+    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app)
     server.serve_forever()
