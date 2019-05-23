dc4297881402a45c802c270c6248ec0780356f07
David Brodsky <dbro@dbro.pro>
2019-05-23 08:28:27 -0700

    Add Kitchen Hall pseudo room

--- settings.py
+++ settings.py
@@ -19,6 +19,12 @@ ROOMS = [
                   motion_pin=6623462419764822, # Zigbee address
                   motion_timeout=datetime.timedelta(minutes=15)),
 
+    # A pseud-room, used to cover exit from Living Room
+    Room(name='Kitchen Hall',
+         lights=[],
+         motion_pin=6623462419764631, # Zigbee address
+         motion_timeout=datetime.timedelta(minutes=5)),
+
     Room(name='Kitchen',
          lights=[3, 20, 14, 15],
          motion_pin=6623462419746382, # Zigbee address
@@ -29,11 +35,10 @@ ROOMS = [
 
 ]
 
-# Room -> Exit Neighbors. All rooms below must have motion detection. All rooms
+# Room -> Exit Neighbors. All rooms below must have motion detection. All key rooms
 # must have no other exits
 ROOM_GRAPH = {
-    'Living Room': ['Kitchen'],
-    'Kitchen': ['Living Room'],
+    'Living Room': ['Kitchen', 'Kitchen Hall'],
 }
 
 ZIGBEE_UDP_PORT = 5005
